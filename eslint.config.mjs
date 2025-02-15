5801b3d7a59f5eccfe2a21ae80b2d6e8e68d129c
allowedcloud <79382276+allowedcloud@users.noreply.github.com>
2025-02-15 17:55:00 -0500

    Add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  // rules: {
+  //     'prefer-const': 'off', // Disable the rule
+      // or
+      // 'prefer-const': ['warn', { 'destructuring': 'all' }] // Adjust the rule settings
+  }
+})
